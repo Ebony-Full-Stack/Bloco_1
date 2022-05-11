8e99e99a62f85a93848bb629fddc0f02e381efd7
leon4rdoalves <leon4rdoalvess@gmail.com>
2022-05-11 12:33:58 -0300

    exe01 is done

---
+++ java/Atividades/05 - Orientação ao Objeto/src/exercício01/Cliente.java
@@ -0,0 +1,43 @@
+package exercício01;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	private String nome, email;
+	private Date dtNascimento;
+	private Integer cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Date date) {
+		this.dtNascimento = date;
+	}
+
+	public Integer getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+	
+}

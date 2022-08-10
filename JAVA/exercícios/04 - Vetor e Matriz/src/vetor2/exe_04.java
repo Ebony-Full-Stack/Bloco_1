d20291144d7f427be0b9c31cf3c2b91c87237f04
leon4rdoalves <leon4rdoalvess@gmail.com>
2022-08-10 19:37:36 -0300

    up to all

--- JAVA/exercícios/04 - Vetor e Matriz/src/vetor2/exe_04.java
+++ JAVA/exercícios/04 - Vetor e Matriz/src/vetor2/exe_04.java
@@ -17,7 +17,7 @@ Autor: Leonardo Alves
 import java.util.Scanner;
 
 public class exe_04 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		double [][] matriz01 = new double[2][2];
 		double [][] matriz02 = new double[2][2];
@@ -31,14 +31,36 @@ public class exe_04 {
 			}
 		}
 		
+// besteirol __________________________________________________________
+		
+		for (int cont=0; cont<43; cont++) {
+			System.out.print(".");
+			Thread.currentThread().sleep(50);
+		}
+		System.out.println("\n      * Bem vinde à Ebony System * ");
+		System.out.println("  Nossa solução é focada na utilização\n"
+				+ "de matrizes para os mais variados cálculos.\n "
+				+ "Utilize esta demo e existindo interesse,\n"
+				+ "     contate-nos por |11| 979714423");
+		for (int cont=0; cont<43; cont++) {
+			System.out.print("_");
+			Thread.currentThread().sleep(50);
+		}
+//______________________________________________________________________
+		
 		while (true) {
 			
-			System.out.print("\nMENU PRINCIPAL\n[ 1 ] Somar\n[ 2 ] Subtrair"
+			System.out.print("\n\nMENU PRINCIPAL\n[ 1 ] Somar\n[ 2 ] Subtrair"
 					+ "\n[ 3 ] Adicionar Constante\n[ 4 ] Imprimir\n[ 5 ] Sair\nOPÇÃO: ");
 			opcao = entrada.nextInt();
 			
 			if (opcao == 1) {
 				System.out.println("\nSomandos os valores das matrizes.");
+				for (int cont=0; cont<33; cont++) {
+					System.out.print(".");
+					Thread.currentThread().sleep(50);
+				}System.out.println();
+				
 				for (int linha=0; linha<2; linha++) {
 					for (int coluna=0; coluna<matriz01[linha].length; coluna++) {
 						System.out.print("[ " + (matriz01[linha][coluna] + matriz02[linha][coluna]) + "] ");
@@ -48,6 +70,10 @@ public class exe_04 {
 			}
 			else if (opcao == 2) {
 				System.out.println("\nSubtraindo os valores das matrizes.");
+				for (int cont=0; cont<35; cont++) {
+					System.out.print(".");
+					Thread.currentThread().sleep(50);
+				}System.out.println();
 				for (int linha=0; linha<2; linha++) {
 					for (int coluna=0; coluna<matriz01[linha].length; coluna++) {
 						System.out.print("[ " + (matriz01[linha][coluna] - matriz02[linha][coluna]) + "] ");
@@ -64,9 +90,9 @@ public class exe_04 {
 				final Double constante  = entrada.nextDouble();			
 				
 				System.out.println("\nAgora, informe a posição para inserir a constante.");
-				System.out.print("\nlinha: ");
+				System.out.print("linha: ");
 				l = entrada.nextInt();
-				System.out.print("\nColuna: ");
+				System.out.print("Coluna: ");
 				c = entrada.nextInt();				
 				
 				if (opcao == 1) {
@@ -86,6 +112,10 @@ public class exe_04 {
 			
 			else if (opcao == 4) {
 				System.out.println("\nImprimindo matriz 01");
+				for (int cont=0; cont<20; cont++) {
+					System.out.print(".");
+					Thread.currentThread().sleep(50);
+				}System.out.println();
 				for (int linha=0; linha<2; linha++) {
 					for (int coluna=0; coluna<matriz01[linha].length; coluna++) {
 						System.out.print("[ " + matriz01[linha][coluna] + " ]");
@@ -94,6 +124,10 @@ public class exe_04 {
 				}
 				
 				System.out.println("\nImprimindo matriz 02");
+				for (int cont=0; cont<20; cont++) {
+					System.out.print(".");
+					Thread.currentThread().sleep(50);
+				}System.out.println();
 				for (int linha=0; linha<2; linha++) {
 					for (int coluna=0; coluna<matriz02[linha].length; coluna++) {
 						System.out.print("[ " + matriz02[linha][coluna] + " ]");
@@ -103,6 +137,7 @@ public class exe_04 {
 			}
 			
 			else if (opcao == 5) {
+				System.out.println("\n");
 				break;
 			}
 			
@@ -110,6 +145,23 @@ public class exe_04 {
 				System.out.println("Ops, opção inválida!");
 			}
 		}
-		System.out.println("\nHeeey, valeu por usar nosso sistema! ;)");
+		
+// besteirol __________________________________________________________
+		
+				for (int cont=0; cont<44; cont++) {
+					System.out.print("*");
+					Thread.currentThread().sleep(50);
+				}
+				System.out.println("\n    Obrigado por utilizar nosso sistema!\n"
+						+ "Quaisquer sugestões sempre serão bem vindas,\n"
+						+ "nos despedimos esperando ter alçançado suas\n"
+						+ "        expectativas e necessidades.\n"
+						+ "              Até a próxima...\n"
+						+ "                -Ebony Sys-");
+				for (int cont=0; cont<44; cont++) {
+					System.out.print("*");
+					Thread.currentThread().sleep(50);
+				}
+//______________________________________________________________________		
 	}
 }
